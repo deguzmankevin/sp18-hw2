7b5763054616dc42ed6dd4b7d7a39be55ba763c3
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-02-17 23:43:37 -0800

    cookie butter is addicting..

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,4 +16,12 @@ class PagesController < ApplicationController
   def person
     @person = Person.new params[:name], params[:age]
   end
+  
+  def me
+    @name = 'Kevin'
+    @year = 'Junior'
+    @from = 'Imperial Beach'
+    @fun = 'I live 10 minutes from the US/Mexico border and I like to tell myself I\'m part Mexican'
+    @excited = 'I\'m most excited to have a ton of fun learning how to make a dynamic website because I\'ve only ever made static websites! Also to see Ken Chen and meet people :)'
+  end
 end
